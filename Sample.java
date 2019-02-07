a48ed6f0c23f1c2ea96012bef96cb7798295b4ce
anudanda503 <chetandanda@gmail.com>
2019-02-06 18:59:50 -0600

    sample java pgm

---
+++ Sample.java
@@ -0,0 +1,22 @@
+package com.basic;
+
+public class Sample {
+
+	public static void main(String[] args) {
+		int a=10;
+		short b=20;
+		double c=10.50;
+		float d=20.50f;
+		char ch='c';
+		 a=b;
+		System.out.println("the value of  addition is:" + (a+b));				
+		System.out.println("the value of addition is:" + (a+d));			
+		System.out.println("the value of c is:" + (ch));
+		System.out.println("the result is:" + ((a!=10)||(b==20)));
+		System.out.println("the value of division is:" + (a/b));
+		System.out.println("the value of division is:" + (b/c));
+		System.out.println("the value of division is:" + (d/c));
+
+	}
+
+}
